399c54f144a48c2f8403bfff1a0c35d25ef6e149
Corey Burmeister <burmeister.corey@gmail.com>
2013-05-01 21:48:57 -0700

    add top, mid, bottom crop types

--- placepuppy/utils.py
+++ placepuppy/utils.py
@@ -3,7 +3,7 @@ from random import choice
 import PIL
 from PIL import Image
 
-def resize_da_puppeh(width, height):
+def resize_da_puppeh(width, height, crop='middle'):
 
     images = glob.glob('placepuppy/static/img/300.png')
     img = Image.open(choice(images))
@@ -14,13 +14,29 @@ def resize_da_puppeh(width, height):
     if ratio > img_ratio:
         img = img.resize((width, width * img.size[1] / img.size[0]),
                 Image.ANTIALIAS)
-        box = (0, 0, img.size[0], height)
+
+        if crop == 'top':
+            box = (0, 0, img.size[0], height)
+        elif crop == 'middle':
+            box = (0, (img.size[1] - height) / 2, img.size[0],
+                    (img.size[1] + height) / 2)
+        else:
+            box = (0, img.size[1] - height, img.size[0], img.size[1])
         img = img.crop(box)
+
     elif ratio < img_ratio:
         img = img.resize((height * img.size[0] / img.size[1], height),
                 Image.ANTIALIAS)
-        box = (0, 0, width, img.size[1])
+
+        if crop == 'top':
+            box = (0, 0, width, img.size[1])
+        elif crop == 'middle':
+            box = ((img.size[0] - width) / 2, 0, (img.size[0] + width) / 2,
+                    img.size[1])
+        else:
+            box = (img.size[0] - width, 0, img.size[0], img.size[1])
         img = img.crop(box)
+
     else:
         img = img.resize((width, height), Image.ANTIALIAS)
     
