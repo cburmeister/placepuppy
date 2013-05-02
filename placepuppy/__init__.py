5c5370a3d0421969103b93d76710ef0544a9f610
Corey Burmeister <burmeister.corey@gmail.com>
2013-05-01 22:04:12 -0700

    enable upload

--- placepuppy/__init__.py
+++ placepuppy/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'placepuppy/static/img'
+
 import logging
 from logging import FileHandler
 file_handler = FileHandler('/tmp/placepuppy.log')
--- placepuppy/views.py
+++ placepuppy/views.py
@@ -1,5 +1,7 @@
-from flask import request, render_template, send_file
+from flask import request, redirect, url_for, render_template, send_file
 from placepuppy import app, utils
+import os
+from werkzeug import secure_filename
 
 @app.route('/', methods=['GET'])
 def index():
@@ -9,3 +11,13 @@ def index():
 def image(width, height):
     utils.resize_da_puppeh(width, height)
     return send_file('static/img/resized_img.png', mimetype='image/png')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
