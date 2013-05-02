44d73b1a0723651223ea5f13a936eb9784cf8463
Corey Burmeister <burmeister.corey@gmail.com>
2013-05-01 19:35:08 -0700

    add views & templates

--- placepuppy/views.py
+++ placepuppy/views.py
@@ -1,6 +1,14 @@
-from flask import request
+import glob
+from random import choice
+from flask import request, render_template, send_file
 from placepuppy import app
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    return 'PUPPIES!!!!'
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/<width>/<height>', methods=['GET'])
+def image(width, height):
+    images = glob.glob('placepuppy/static/img/*')
+    src = choice(images)[11:]
+    return send_file(src, mimetype='image/png')
